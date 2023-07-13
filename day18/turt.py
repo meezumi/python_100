300a333578642872c41f4310bf2d4926a86819a4
aark07 <aaryank098@gmail.com>
2023-07-13 22:04:36 +0530

    day 18

---
+++ day18/turt.py
@@ -0,0 +1,46 @@
+from turtle import Turtle, Screen
+
+
+# timmy = Turtle()
+# # new turtle object 
+
+# # to make a square 
+# for move in range(4):
+#   timmy.forward(100)
+#   timmy.right(90)
+  
+# we can give alias to the modules imported 
+import turtle as t
+
+tim = t.Turtle()
+
+import heroes
+print(heroes.gen())
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
