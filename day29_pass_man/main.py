11a2f37f155982cdede74293beaed198ff07d431
aark07 <aaryank098@gmail.com>
2023-10-21 11:57:11 +0530

    till day31

--- day29_pass_man/main.py
+++ day29_pass_man/main.py
@@ -1,5 +1,16 @@
+from tkinter import *
+
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Password Manager")
+window.config(padx=20, pady=20)
+
+canvas = Canvas(width=200, height=200)
+rimuru_img = PhotoImage(file="rimuru.jpg")
+canvas.create_image(100, 100)
